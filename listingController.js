7a87a71edef96fc42440a8badd7ea1880731e5e2
Eric Suarez <egs104@gmail.com>
2016-01-15 12:15:40 -0500

    fixed small error on delete button

--- listingController.js
+++ listingController.js
@@ -24,10 +24,12 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       })
 
     };
+
     $scope.deleteListing = function(listing) {
       var index = $scope.listings.indexOf(listing);
       $scope.listings.splice(index,1);
     };
+
     $scope.showDetails = function(index) {
 
       $scope.detailedInfo = {
